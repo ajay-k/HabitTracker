03de684fdbe2925df0adba0cb2b415b0c91b9aa5
ajay-k <ajay_k@sbcglobal.net>
2020-05-03 16:35:59 -0700

    Config deployment

--- habittracker/settings.py
+++ habittracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/habits/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
